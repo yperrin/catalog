a250a5c09f191bcce8deceb865707e5f8cb7973d
Yann <yann.perrin@clarivate.com>
2025-10-30 21:50:46 -0400

    making the project zoneless

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,16 +1,20 @@
 import { TestBed } from '@angular/core/testing';
 import { App } from './app';
+import { NxWelcome } from './nx-welcome';
 
 describe('App', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [App],
+      imports: [App, NxWelcome],
     }).compileComponents();
   });
 
-  it('should create the app', () => {
+  it('should render title', () => {
     const fixture = TestBed.createComponent(App);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      'Welcome catalog'
+    );
   });
 });
